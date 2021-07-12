d8cd2a155dea58017b7e0deb220c9fdf2d3d2156
alyson <alysonalyson7@hotmail.com>
2021-07-12 16:33:22 -0300

    criptografia :)

--- src/main/java/com/matchactivities/springbootbackend/controller/UserController.java
+++ src/main/java/com/matchactivities/springbootbackend/controller/UserController.java
@@ -3,6 +3,7 @@ package com.matchactivities.springbootbackend.controller;
 import com.matchactivities.springbootbackend.model.RegisterForm;
 import com.matchactivities.springbootbackend.model.User;
 import com.matchactivities.springbootbackend.repository.UserRepository;
+import com.matchactivities.springbootbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,9 @@ import java.util.List;
 public class UserController {
 
     @Autowired
+    UserService userService;
+
+    @Autowired
     UserRepository userRepository;
 
     @GetMapping
@@ -25,11 +29,12 @@ public class UserController {
     @PostMapping()
     public ResponseEntity<String> register(@RequestBody RegisterForm newUser) {
 
+         User user = userService.registerNewUser(newUser);
 
         // kkkk nao tem metodo de validacao ainda
-        User user = new User(newUser.getName(), newUser.getEmail(), newUser.getPassword());
-        userRepository.save(user);
-        return ResponseEntity.ok("usuario cadastrado");
+
+
+        return ResponseEntity.ok("Usuário cadastrado");
     }
 
 
--- src/main/java/com/matchactivities/springbootbackend/service/UserService.java
+++ src/main/java/com/matchactivities/springbootbackend/service/UserService.java
@@ -1,18 +1,33 @@
 package com.matchactivities.springbootbackend.service;
 
+import com.matchactivities.springbootbackend.model.RegisterForm;
+import com.matchactivities.springbootbackend.model.User;
 import com.matchactivities.springbootbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Properties;
+
 
 @Service
 public class UserService {
-    private UserRepository userRepository;
 
+    @Autowired
+    UserRepository userRepository;
+    
     PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     //metodo de validacao de formulário da zueira
 
 
+    //metodo de salvar usuario
+
+    public User registerNewUser(RegisterForm newUser){
+
+    User user = new User();
+    newUser.getName(); newUser.getEmail(); user.setPassword(passwordEncoder.encode(newUser.getPassword()));
+       return userRepository.save(user) ;
+    }
 }
